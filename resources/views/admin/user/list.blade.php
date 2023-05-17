9b27407e95a2bcd2bc9722e470bd4ec9dc568ba3
isdprj <isdgrpprj@gmail.com>
2023-05-17 13:48:11 +0700

    Minh
    Thay đổi sang tiếng việt

--- resources/views/admin/user/list.blade.php
+++ resources/views/admin/user/list.blade.php
@@ -3,7 +3,7 @@
 @section('content')
         <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Tìm kiếm..." aria-label="Search">
                 <div class="input-group-append">
                     <button class="btn btn-sidebar">
                         <i class="fas fa-search fa-fw"></i>
