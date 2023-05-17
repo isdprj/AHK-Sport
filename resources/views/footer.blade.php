1556001d68293cd5957e60990ec1dca2dc5810c1
isdprj <128308233+isdprj@users.noreply.github.com>
2023-05-17 20:27:50 +0700

    add_LikeFace

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,44 +1,47 @@
 <div id="footer" class="color-div">
     <div class="container">
         <div class="row">
-            <div class="col-sm-3">
+        <div class="col-sm-3">
                 <div class="widget">
                     <h4 class="widget-title">Cộng đồng</h4>
                     <div id="social-lists">
                         <div>
-                            <a href="#"><i class="social-size ti-facebook"></i></a>
+                            <a href="https://www.facebook.com/profile.php?id=100092511921797"><i class="social-size ti-facebook"></i></a>
                             <a href="#"><i class="social-size ti-instagram"></i> </a>
-                            <a href="#"><i class="social-size ti-twitter"></i> </a>
-                            <a href="#"><i class="social-size ti-linkedin"></i> </a>
                         </div>
                     </div>
                 </div>
             </div>
+
             <div class="col-sm-4">
                 <div class="widget">
-                    <h4 class="widget-title">Địa chỉ</h4>
+                    <h4 class="widget-title">BẢN ĐỒ</h4>
                     <div>
-                        <div class="map-sizing wow flipInX"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.446504973986!2d105.77936307512884!3d21.014812980631046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313453366bf9ef75%3A0x6698b35be1c121ff!2sVTI%20Building!5e0!3m2!1svi!2s!4v1682522308560!5m2!1svi!2s"></iframe></div>
+                            <div class="map-sizing wow flipInX"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.446504973986!2d105.77936307512884!3d21.014812980631046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313453366bf9ef75%3A0x6698b35be1c121ff!2sVTI%20Building!5e0!3m2!1svi!2s!4v1682522308560!5m2!1svi!2s"></iframe></div>
                     </div>
                 </div>
             </div>
+
+
+
             <div class="col-sm-3">
-                <div class="col-sm-10">
                 <div class="widget">
-                        <h4 class="widget-title">Liên hệ</h4>
-                        <div>
+                    <h4 class="widget-title">Liên hệ</h4>
+                    <div>
                             <p><i class="fa fa-map-marker"></i>&ensp;Địa chỉ: Khu đô thị Mễ Trì Hạ, Nam Từ Liêm, Hà Nội </p>
                             <br>
                             <p><i class="fa fa-phone"></i>&ensp;Điện thoại: +84 328 047 403</p>
                             <br>
                             <p><i class="fa fa-google-plus"></i>&ensp;Email: ahksport@gmail.com</p>
-                        </div>
                     </div>
                 </div>
             </div>
+
+        
+
             <div class="col-sm-2">
                 <div class="widget">
-                    <h4 class="widget-title">Cập nhật tin tức</h4>
+                    <h4 class="widget-title">CẬP NHẬT TIN TỨC</h4>
                     <form action="javascript:void(0);" method="get">
                         <input type="email" name="your_email" placeholder="Email">
                         <button class="beta-btn primary" type="submit" onclick="showPopup()">Đăng ký<i class="fa fa-chevron-right"></i></button>
@@ -52,6 +55,8 @@
                         }
                     </script>
                 </div>
+                <br>
+               
             </div>
         </div> <!-- .row -->
     </div> <!-- .container -->
